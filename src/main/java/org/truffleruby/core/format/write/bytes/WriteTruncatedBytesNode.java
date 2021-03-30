071f8bb593d07639277681148793cc6c993639a1
Kir Shatrov <shatrov@me.com>
2021-03-30 23:00:51 +0100

    WIP

--- src/main/java/org/truffleruby/core/format/write/bytes/WriteTruncatedBytesNode.java
+++ src/main/java/org/truffleruby/core/format/write/bytes/WriteTruncatedBytesNode.java
@@ -10,26 +10,11 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 
 @NodeChild("value")
 public abstract class WriteTruncatedBytesNode extends FormatNode {
-//    private final Integer size;
-
-//    public WriteTruncatedBytesNode(Integer size) {
-//        this.size = size;
-//    }
 
     @Specialization
     protected Object write(VirtualFrame frame, byte[] bytes) {
         writeBytes(frame, bytes);
         return null;
     }
-//
-//    private Object write(VirtualFrame frame, byte[] bytes) {
-//        writeBytes(frame, bytes);
-
-//        for (int n = 0; n < size; n++) {
-//            writeByte(frame, bytes[n]);
-//        }
-//
-//        return null;
-//    }
 
 }
