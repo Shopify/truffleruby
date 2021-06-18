719cdc740ef0c588600cae41789288df651f30e2
wildmaples <maple.ong@shopify.com>
2021-06-18 13:46:17 -0400

    Add benchmark for keyword arguments (without defaults)

--- bench/micro/truffleruby/keyword_arguments.rb
+++ bench/micro/truffleruby/keyword_arguments.rb
@@ -6,22 +6,30 @@ puts RUBY_DESCRIPTION
 # rubocop:disable Metrics/ParameterLists
 
 class A 
-  def with_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10)
+  def keyword_with_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10)
     a + b + c + d + e + f + g + h + i + j
   end
 
-  def without_defaults(a, b, c, d, e, f, g, h, i, j)
+  def keyword_without_defaults(a:, b:, c:, d:, e:, f:, g:, h:, i:, j:)
+    a + b + c + d + e + f + g + h + i + j
+  end
+
+  def positional_without_defaults(a, b, c, d, e, f, g, h, i, j)
     a + b + c + d + e + f + g + h + i + j
   end
 end
 
 class B
-  def with_defaults(j: 11, i: 12, h: 13, g: 14, f: 15, e: 16, d: 17,
+  def keyword_with_defaults(j: 11, i: 12, h: 13, g: 14, f: 15, e: 16, d: 17,
     c: 18, b: 19, a: 20)
       a + b + c + d + e + f + g + h + i + j
   end
 
-  def without_defaults(a, b, c, d, e, f, g, h, i, j)
+  def keyword_without_defaults(j:, i:, h:, g:, f:, e:, d:, c:, b:, a:)
+    a + b + c + d + e + f + g + h + i + j
+  end
+
+  def positional_without_defaults(a, b, c, d, e, f, g, h, i, j)
     a + b + c + d + e + f + g + h + i + j
   end
 end
@@ -40,19 +48,29 @@ def obj_a_or_b
 end
 
 Benchmark.ips do |x|
-  x.report('Monomorphic, with default args') do
-    obj_a.with_defaults
+  x.report('Monomorphic, keyword with default args') do
+    obj_a.keyword_with_defaults
   end
 
-  x.report('Monomorphic, without default args') do
-    obj_b.without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  x.report('Monomorphic, keyword without default args') do
+    obj_a.keyword_without_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, 
+      f: 6, g: 7, h: 8, i: 9, j: 10)
   end
 
-  x.report('Polymorphic, with default args') do
-    obj_a_or_b.with_defaults
+  x.report('Monomorphic, positional without default args') do
+    obj_b.positional_without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   end
 
-  x.report('Polymorphic, without default args') do
-    obj_a_or_b.without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  x.report('Polymorphic, keyword with default args') do
+    obj_a_or_b.keyword_with_defaults
+  end
+
+  x.report('Polymorphic, keyword without default args') do
+    obj_a_or_b.keyword_without_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, 
+      f: 6, g: 7, h: 8, i: 9, j: 10)
+  end
+
+  x.report('Polymorphic, positional without default args') do
+    obj_a_or_b.positional_without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
   end
 end
