8f456d290fc88da6d894153f484c3716a1da03b2
wildmaples <maple.ong@shopify.com>
2021-06-29 17:25:18 -0400

    Modify benchmark for small number of keyword arguments

--- bench/micro/truffleruby/keyword_arguments.rb
+++ bench/micro/truffleruby/keyword_arguments.rb
@@ -6,31 +6,30 @@ puts RUBY_DESCRIPTION
 # rubocop:disable Metrics/ParameterLists
 
 class A 
-  def keyword_with_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10)
-    a + b + c + d + e + f + g + h + i + j
+  def keyword_with_defaults(a: 1, b: 2, c: 3)
+    a + b + c
   end
 
-  def keyword_without_defaults(a:, b:, c:, d:, e:, f:, g:, h:, i:, j:)
-    a + b + c + d + e + f + g + h + i + j
+  def keyword_without_defaults(a:, b:, c:)
+    a + b + c
   end
 
-  def positional_without_defaults(a, b, c, d, e, f, g, h, i, j)
-    a + b + c + d + e + f + g + h + i + j
+  def positional_without_defaults(a, b, c)
+    a + b + c
   end
 end
 
 class B
-  def keyword_with_defaults(j: 11, i: 12, h: 13, g: 14, f: 15, e: 16, d: 17,
-    c: 18, b: 19, a: 20)
-      a + b + c + d + e + f + g + h + i + j
+  def keyword_with_defaults(c: 18, b: 19, a: 20)
+    a + b + c
   end
 
-  def keyword_without_defaults(j:, i:, h:, g:, f:, e:, d:, c:, b:, a:)
-    a + b + c + d + e + f + g + h + i + j
+  def keyword_without_defaults(c:, b:, a:)
+    a + b + c
   end
 
-  def positional_without_defaults(a, b, c, d, e, f, g, h, i, j)
-    a + b + c + d + e + f + g + h + i + j
+  def positional_without_defaults(a, b, c)
+    a + b + c
   end
 end
 
@@ -53,12 +52,11 @@ Benchmark.ips do |x|
   end
 
   x.report('Monomorphic, keyword without default args') do
-    obj_a.keyword_without_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, 
-      f: 6, g: 7, h: 8, i: 9, j: 10)
+    obj_a.keyword_without_defaults(a: 1, b: 2, c: 3)
   end
 
   x.report('Monomorphic, positional without default args') do
-    obj_b.positional_without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    obj_b.positional_without_defaults(1, 2, 3)
   end
 
   x.report('Polymorphic, keyword with default args') do
@@ -66,11 +64,10 @@ Benchmark.ips do |x|
   end
 
   x.report('Polymorphic, keyword without default args') do
-    obj_a_or_b.keyword_without_defaults(a: 1, b: 2, c: 3, d: 4, e: 5, 
-      f: 6, g: 7, h: 8, i: 9, j: 10)
+    obj_a_or_b.keyword_without_defaults(a: 1, b: 2, c: 3)
   end
 
   x.report('Polymorphic, positional without default args') do
-    obj_a_or_b.positional_without_defaults(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    obj_a_or_b.positional_without_defaults(1, 2, 3)
   end
 end
