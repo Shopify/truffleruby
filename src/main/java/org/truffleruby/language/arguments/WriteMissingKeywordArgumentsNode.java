8e74a6dc2d5840e14dd1517c0691bcd34e31e7e0
wildmaples <maple.ong@shopify.com>
2021-08-23 12:02:20 -0700

    Add dimensions to frameSlots

--- src/main/java/org/truffleruby/language/arguments/WriteMissingKeywordArgumentsNode.java
+++ src/main/java/org/truffleruby/language/arguments/WriteMissingKeywordArgumentsNode.java
@@ -1,5 +1,6 @@
 package org.truffleruby.language.arguments;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
@@ -9,6 +10,7 @@ public class WriteMissingKeywordArgumentsNode extends RubyContextSourceNode {
 
     public static final Object MISSING = new Object();
 
+    @CompilerDirectives.CompilationFinal(dimensions = 1)
     private final FrameSlot[] frameSlots;
 
     public WriteMissingKeywordArgumentsNode(FrameSlot[] frameSlots) {
